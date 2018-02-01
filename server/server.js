1ffa394b48ac28467b10f4ab757fab5a2cabd1a7
Chris Pratt <croxxp@gmail.com>
2018-01-31 21:29:13 -0500

    Basic server start.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+
+const fs = require('fs');
+
+app.use(parser.json());
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => fs.createReadStream('dist/index.html').pipe(res));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
