2e21b4405afc20cf3e89d4e2a737787f7531239d
tsterne <tsterne@allegient.com>
2018-02-03 21:43:16 -0500

    Fixing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,46 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/never';
+
+import { SuppliesService } from './shared/services/supplies.service';
+import { Supply } from './shared/models/supply';
+import { create } from 'domain';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        {
+          provide: SuppliesService,
+          useValue: {
+            create: (supply: Supply) => Observable.never(),
+            getAll: () => Observable.never(),
+          },
+        }
+      ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+
+  it('should call supplies service', async(() => inject([SuppliesService], (svc) => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const createSpy = spyOn(svc, 'create');
+    const getSpy = spyOn(svc, 'getAll');
+
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
+    expect(createSpy).toHaveBeenCalled();
+    expect(getSpy).toHaveBeenCalled();
+  })));
 });
