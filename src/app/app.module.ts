22031c75d0cdc8809814a2acd8c095c8c007c9b7
tsterne <tsterne@allegient.com>
2018-02-10 23:05:19 -0500

    Added a side bar and a header... kind of

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatCardModule, MatProgressBarModule } from '@angular/material';
 
 @NgModule({
     exports: [
         CommonModule,
+        MatButtonModule,
+        MatCardModule,
+        MatProgressBarModule,
     ]
 })
-export class SharedModule {
-
-}
+export class SharedModule { }
