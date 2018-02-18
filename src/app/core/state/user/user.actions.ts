098f24e616311095f079f0da3d5613f02ec2d805
tsterne <tsterne@allegient.com>
2018-02-18 15:48:50 -0500

    Removing unused action

--- src/app/core/state/user/user.actions.ts
+++ src/app/core/state/user/user.actions.ts
@@ -1,11 +1,6 @@
 import { Action } from '@ngrx/store';
 
-export const SAVE_USER = '[User] Save User';
 export const SET_USER = '[User] Set User';
-
-export class SaveUser implements Action {
-  readonly type = SAVE_USER;
-}
 
 export class SetUser implements Action {
   readonly type = SET_USER;
@@ -13,4 +8,4 @@ export class SetUser implements Action {
   constructor(public name: string, public logo: string) {}
 }
 
-export type Actions = SaveUser | SetUser;
+export type Actions = SetUser;
