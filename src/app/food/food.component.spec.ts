5aa47366c50af2860c3965d1a0a1a4b600bdd70c
tsterne <tsterne@allegient.com>
2018-02-24 17:47:33 -0500

    fixing tests

--- src/app/food/food.component.spec.ts
+++ src/app/food/food.component.spec.ts
@@ -1,17 +1,21 @@
+import { TestModule } from './../../test/test.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FoodComponent } from './food.component';
+import { MatCardModule } from '@angular/material';
 
 describe('FoodComponent', () => {
   let component: FoodComponent;
   let fixture: ComponentFixture<FoodComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FoodComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [FoodComponent],
+        imports: [MatCardModule, TestModule]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FoodComponent);
