953568f84a3c8c9c99d12d6acf47da3acab2bf7a
Chris Pratt <croxxp@gmail.com>
2018-02-28 22:06:24 -0500

    Added new router.

--- server/server.js
+++ server/server.js
@@ -9,20 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(parser.json());
 app.use(express.static('dist'));
 
-const loki = require('lokijs');
-const db = new loki('db.js');
-
-var supplies = db.addCollection('supplies');
+app.use('/food', require('./food'));
 
 app.get('/', (req, res) => fs.createReadStream('dist/index.html').pipe(res));
-
-app.get('/supplies', (req, res) => {
-    res.send(supplies.find());
-});
-
-app.post('/supplies', (req, res) => {
-    const id = supplies.insert(req.body).$loki;
-    res.send(id);
-})
 
 app.listen(port, () => console.log(`Server running: PORT ${port}`))
