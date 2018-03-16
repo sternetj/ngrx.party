9324aa99a96a1613539acb47dc6c08da2c3a2ed4
Chris Pratt <croxxp@gmail.com>
2018-03-16 19:38:25 -0400

    Oops

---
+++ src/app/core/state/food/food.selectors.ts
@@ -0,0 +1,22 @@
+import { AppState } from '../index';
+
+import { createSelector } from '@ngrx/store';
+
+const selectAppState = (state: AppState) => state;
+
+export const foodSelectors = {
+    counts: createSelector(selectAppState, (state: AppState) => {
+        let totalCount = 0;
+        let bringingCount = 0;
+
+        state.food.food.forEach((food) => {
+            totalCount += food.count;
+            bringingCount += food.users.length;
+        });
+
+        return {
+         total: totalCount,
+         bringing: bringingCount,
+        };
+    }),
+}
